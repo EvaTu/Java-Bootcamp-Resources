4270f1ccc196c3a00ab7baf364bf19cc2daec442
EvaTu <evatuw@gmail.com>
2022-07-11 14:29:35 -0400

    completed 'Getting Started'

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,11 @@
+
 public class YourInitials {
     public static void main(String[] args) {
         //print initials here. 
+        System.out.println("EEEEE\tTTTTTTT");
+        System.out.print("E\t   T");
+        System.out.println("\nEEE\t   T");
+        System.out.print("E\t   T");
+        System.out.println("\nEEEEE\t   T");
     }
 }
--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -9,3 +9,9 @@
                                               // "Java > Python."
                                               // "I spilled Java all over my paper."
                                               // "My dog ate my Java." 
+public class FirstProgram{
+
+    public static void main(String[] args) {
+        System.out.print("Printing is fun!\n"+"Java > Python.\n"+"I spilled Java all over my paper.\n"+"My dog ate my Java.");
+    }
+}
